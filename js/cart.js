248930140a23ea60be750ab841dd8ac68caad095
vtr363 <victor.m.rocha.vr@gmail.com>
2022-11-26 11:47:03 -0300

    feat: atualização de total e subtotal

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,31 @@
-var valorTotal = [0,0];
-var valorProduto = [50.00, 30.00];
-var qtd = [0,0];
+let valorTotal = [0,0];
+let valorProduto = [50.00, 30.00];
+let qtd = [0,0];
+
 
 function adcionarItem(item){
-    var quantidade = document.getElementById('quantidade' + item)
+    let quantidade = document.getElementById('quantidade' + item)
     qtd[item] += 1;
     quantidade.innerHTML = qtd[item]
+    atualizaTotalProduto(item)
+    atualizaSubTotal()
 }
 function removeItem(item){
-    var quantidade = document.getElementById('quantidade' + item)
-    qtd[item] -= 1;
-    quantidade.innerHTML = qtd[item]
+    let quantidade = document.getElementById('quantidade' + item)
+    if (qtd[item] > 0){
+        qtd[item] -= 1;
+        quantidade.innerHTML = qtd[item]
+        atualizaTotalProduto(item)
+        atualizaSubTotal()
+    }
+    
+}
+function atualizaTotalProduto(item){
+    let total = document.getElementById('total' + item)
+    valorTotal[item] = valorProduto[item] * qtd[item]
+    total.innerHTML = valorTotal[item]
+}
+function atualizaSubTotal(){
+    let totalCompra = document.getElementById('valorTotalCompra')
+    totalCompra.innerHTML = valorTotal.reduce((soma, a) => soma + a, 0)
 }
