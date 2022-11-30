df6f00efcfc4f75f3b2071e016d4ecd52d99995f
vtr363 <victor.m.rocha.vr@gmail.com>
2022-11-30 20:14:28 -0300

    feat: criação dinamica de produtos

--- js/cart.js
+++ js/cart.js
@@ -1,27 +1,29 @@
 let carrinho = []
 
-document.onload = () => {
+function start(){
 
     adicionaAoCarrinho(
         "COTTON T-SHIRT", 
-        "https\://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         "091091001",
         "White",
         "M",
         50.00
     )
-    console.log(carrinho)
+    
     adicionaAoCarrinho(
         "WHITE T-SHIRT", 
-        "https\://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         "056289891",
         "White",
         "XL",
         30.00
     )
     
+    
     atualizaCarrinho()
 }
+
 
 
 function adicionaAoCarrinho(nome, imgLink, artNo, cor, tamanho, preco){
@@ -38,58 +40,64 @@ function adicionaAoCarrinho(nome, imgLink, artNo, cor, tamanho, preco){
 }
 
 function atualizaCarrinho(){
+    
     let carrinhoHtml = document.getElementById("carrinho")
+    
     carrinho.forEach((produto,index) => {
-         carrinhoHtml.appendChild(
+        
+        carrinhoHtml.innerHTML +=
         `<div
-            id=\"produto${index}\"
-            class=\"d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile\"
+            id='produto${index}'
+            class='d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile'
         >
-            <div class=\"d-flex flex-row align-items-center\">
+            <div class='d-flex flex-row align-items-center'>
                 <div>
                     <img
-                        src=\"${produto.imgLink}\"
-                        width=\"150\"
-                        height=\"150\"
-                        alt=\"\"
-                        id=\"image\"
+                        src='${produto.imgLink}'
+                        width='150'
+                        height='150'
+                        alt=''
+                        id='image'
                     />
                 </div>
-                <div class=\"d-flex flex-column pl-md-3 pl-1\">
+                <div class='d-flex flex-column pl-md-3 pl-1'>
                     <div><h6>${produto.nome}</h6></div>
-                    <div>Art.No:<span class=\"pl-2\">${produto.artNo}</span></div>
-                    <div>Color:<span class=\"pl-3\">${produto.cor}</span></div>
-                    <div>Size:<span class=\"pl-4\">${produto.tamanho}</span></div>
+                    <div>Art.No:<span class='pl-2'>${produto.artNo}</span></div>
+                    <div>Color:<span class='pl-3'>${produto.cor}</span></div>
+                    <div>Size:<span class='pl-4'>${produto.tamanho}</span></div>
                 </div>
             </div>
-            <div class=\"pl-md-0 pl-1\"><b>R$ ${produto.preco}}</b></div>
-            <div class=\"pl-md-0 pl-2\">
+            <div class='pl-md-0 pl-1'><b>R$ ${produto.preco.toFixed(2)}</b></div>
+            <div class='pl-md-0 pl-2'>
                 <span
-                    class=\"fa fa-minus-square text-secondary\"
-                    onclick=\"removeItem(${index})\"
+                    class='fa fa-minus-square text-secondary'
+                    onclick='removeItem(${index})'
                 ></span
-                ><span class=\"px-md-3 px-1\" id=\"quantidade${index}\">${produto.quantidade}</span
+                ><span class='px-md-3 px-1' id='quantidade${index}'>${produto.quantidade}</span
                 ><span
-                    class=\"fa fa-plus-square text-secondary\"
-                    onclick=\"adcionarItem(${index})\"
+                    class='fa fa-plus-square text-secondary'
+                    onclick='adcionarItem(${index})'
                 ></span>
             </div>
-            <div class=\"pl-md-0 pl-1\">
-                <b>R$ <span id=\"total${index}\">0</span></b>
+            <div class='pl-md-0 pl-1'>
+                <b>R$ <span id='total${index}'>0</span></b>
             </div>
-            <div class=\"close\" onclick=\"removeProduto(${index})\">&times;</div>
+            <div class='close' onclick='removeProduto(${index})'>&times;</div>
         </div>`
-        )
+        
+        
     });
-    document.getElementById("carrinho").innerHTML = htmlAux
+    
 }
 
 function adcionarItem(item){
+    console.log(item)
     let qtd = document.getElementById('quantidade' + item)
     let produto = carrinho[item]
     produto.quantidade += 1;
+    qtd.innerHTML = produto.quantidade
 
-    atualizaNumerosTela(qtd)
+    atualizaNumerosTela(qtd, item)
 }
 
 function removeItem(item){
@@ -97,13 +105,14 @@ function removeItem(item){
     let produto = carrinho[item]
     if (produto.quantidade > 0){
         produto.quantidade -= 1;
+        qtd.innerHTML = produto.quantidade
 
-        atualizaNumerosTela(qtd)
+        atualizaNumerosTela(qtd, item)
     }
     
 }
 
-function atualizaNumerosTela(qtd){
+function atualizaNumerosTela(qtd, item){
     qtd.innerHTML = carrinho[item].quantidade
     atualizaTotalProduto(item)
     atualizaSubTotal()
@@ -122,7 +131,7 @@ function atualizaSubTotal(){
     let totalCompra = document.getElementById('valorTotalCompra')
     let subtotal = 0
     carrinho.forEach(produto => {
-        subtotal += produto.quantidade
+        subtotal += produto.quantidade * produto.preco
     });
     totalCompra.innerHTML = subtotal.toFixed(2)
 }
